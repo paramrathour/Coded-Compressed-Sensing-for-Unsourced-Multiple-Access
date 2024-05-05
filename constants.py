60bad2cad1e0adb97b52c0b705f2b5397d1f695e
paramrathour <rathourparam3435@gmail.com>
2024-05-05 12:38:41 +0530

    added more constants

--- constants.py
+++ constants.py
@@ -1,10 +1,21 @@
+import numpy as np
+
 RANDOM_SEED = 42 # Set this to None for truly (pseudo)random behavior.
 
 B = 75
 
 n = 11
-K = 25
+Ka = 25
+Kd = 10
+K = Ka + Kd
+
 J = 14
 M = n*J
 
 EPSILON_TREE = 0.0025
+# EPSILON_CS = 1e-3
+
+BCH = (2047,23) # (Length, Field) of the Bose–Chaudhuri–Hocquenghem code
+N = BCH[0] * n
+
+A = np.zeros(N/n, 2**J)
